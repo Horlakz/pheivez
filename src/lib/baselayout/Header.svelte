d5a7e0b61fc729c2983b9560a407db79e7a78015
Horlakz <officialhorlakz@gmail.com>
2022-09-21 22:21:56 +0100

    styled page header

--- src/lib/baselayout/Header.svelte
+++ src/lib/baselayout/Header.svelte
@@ -1,17 +1,27 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
 <header class="flex justify-between px-10 py-4 bg-gray-50 border-b">
-  <a href="/">Pheivez</a>
+  <a href="/"><h1 class="text-gray-700 font-semibold text-xl">Pheivez</h1></a>
 
   <nav data-sveltekit-prefetch>
     <ul class="flex gap-2">
-      <li>
+      <li class:active={$page.url.pathname === "/"}>
         <a href="/">Home</a>
       </li>
-      <li>
+      <li class:active={$page.url.pathname === "/about"}>
         <a href="/about">About</a>
       </li>
-      <li>
+      <li class:active={$page.url.pathname === "/todos"}>
         <a href="/todos">Todos</a>
       </li>
     </ul>
   </nav>
 </header>
+
+<style lang="scss">
+  .active {
+    @apply text-gray-700 font-semibold;
+  }
+</style>
