4e07576eda05ac802e46d8b9d01597ac9150831f
Horlakz <officialhorlakz@gmail.com>
2022-09-21 22:13:00 +0100

    created file for baselayout components and styled them

---
+++ src/lib/baselayout/Footer.svelte
@@ -0,0 +1,3 @@
+<footer class="flex-center border-t py-4 bg-gray-50">
+  <p>Copyright &copy; {new Date().getFullYear()}, Pheivez Arts</p>
+</footer>
---
+++ src/lib/baselayout/Header.svelte
@@ -0,0 +1,17 @@
+<header class="flex justify-between px-10 py-4 bg-gray-50 border-b">
+  <a href="/">Pheivez</a>
+
+  <nav data-sveltekit-prefetch>
+    <ul class="flex gap-2">
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <a href="/todos">Todos</a>
+      </li>
+    </ul>
+  </nav>
+</header>
