4f62193054c5a51895b75d375c45e9ace9c04871
Horlakz <officialhorlakz@gmail.com>
2022-09-22 19:34:51 +0100

    created folder for logout page

---
+++ src/routes/logout/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { onMount } from "svelte";
+
+  import Storage from "$lib/utilities/storage";
+
+  const storage = new Storage();
+
+  const logout = () => {
+    storage.remove("token");
+    goto("/");
+  };
+
+  onMount(() => {
+    logout();
+  });
+</script>
